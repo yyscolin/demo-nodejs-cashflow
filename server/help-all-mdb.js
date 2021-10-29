8718168f89a043a6c9e4a3e78102aab20f911a41
yyscolin <48078302+yyscolin@users.noreply.github.com>
2021-10-29 08:15:16 +0800

    Port number for mysql is now configurable

--- server/help-all-mdb.js
+++ server/help-all-mdb.js
@@ -3,6 +3,7 @@ const mysql = require('mysql');
 const con = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
+  port: process.env.MYSQL_PORT,
   password: process.env.MYSQL_PASS
 })
 
