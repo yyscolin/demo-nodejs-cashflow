c6fd520ac92e9d1c4c9470b7e4b9acaf0ddf4b4a
yyscolin <48078302+yyscolin@users.noreply.github.com>
2022-07-07 12:29:19 +1000

    Bugfix - Ext form page tft's field will now show recorded value properly if more than one word

--- server/page-exp11-exts_form.js
+++ server/page-exp11-exts_form.js
@@ -1,36 +1,23 @@
 const mdb = require('./help-all-mdb')
-const getSynsV2 = require('./help-exp11-getSynsV2')
 const handleError = require('./help-all-handleError.js')
 
 module.exports = async function(req, res) {
   try {
-    let id = req.params.id
-    if (id != undefined) {
-      if (id != parseInt(id)) throw new Error('400::Page not found')
-      var dbResponse = await mdb.get(`select date_format(date, '%Y-%m-%d') as date, name as tft, acc, amt, remarks
+    let ext = {}
+    let isNew = true
+
+    const {id: extId} = req.params
+    if (extId != undefined) {
+      if (extId != parseInt(extId)) throw new Error('400::Page not found')
+      ext = await mdb.get(`select exts.id, date_format(date, '%Y-%m-%d') as date, name as tft, acc, amt, remarks
         from exts join tfts on exts.tft = tfts.id
-        where exts.id = ${parseInt(id)}`)
-      if (!dbResponse) throw new Error('400::Page not found')
-      var ext = {
-        id,
-        date: ` value=${dbResponse.date}`,
-        tft: dbResponse.tft ? ` value=${dbResponse.tft}` : '',
-        amt: ` value=${dbResponse.amt}`,
-        remarks: dbResponse.remarks
-      }
-      var isNew = false
-    } else {
-      var ext = {
-        id,
-        date: '',
-        tft: '',
-        amt: '',
-        remarks: ''
-      }
-      var isNew = true
+        where exts.id = ${parseInt(extId)}`)
+      if (!ext) throw new Error('400::Page not found')
+      isNew = false
     }
+
     let accs = await mdb.postQuery(`select id, name from accs order by name`)
-    accs.forEach(acc => acc.selected = (isNew && acc.id==7) || (!isNew && acc.id==dbResponse.acc) ? ` selected='selected'` : '')
+    accs.forEach(acc => acc.selected = (isNew && acc.id==7) || (!isNew && acc.id==ext.acc) ? ` selected='selected'` : '')
     let tfts = await getSynsV2('tft', ext.tft)
     res.render('exts_form', { ext, tfts, accs, isNew })
   } catch(err) {
